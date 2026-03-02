02a648141ce162a516e8eb506bb31bdf1f04ac08
shubh165 <shubhendratiwari165@gmail.com>
2026-03-02 19:26:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+// const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World! This is my first backend project.");
+});
+app.get("/login", (req, res) => {
+  res.send("This is login page.");
+});
+app.get("/about", (req, res) => {
+  res.send("This is about page.");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on localhost:${process.env.PORT}`);
+});
